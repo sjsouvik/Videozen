80db21c661279ee67677a0554774b61e972dae57
Souvik Jana <souvikjana2013@gmail.com>
2021-05-07 15:27:45 +0530

    feat: added model for videos

---
+++ videozen-backend/models/video.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+require("mongoose-type-url");
+
+const videoScheema = new mongoose.Schema(
+  {
+    videoId: {
+      type: String,
+      required: [true, "Please enter video id"],
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Please enter title"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter description"],
+    },
+    ownerImage: {
+      type: mongoose.SchemaTypes.Url,
+      required: [true, "Please enter owner's image url"],
+    },
+    image: {
+      type: mongoose.SchemaTypes.Url,
+      required: [true, "Please enter thumbnail image url"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("video", videoSchema);
