bad61e7228a4f7e8aac26b221f9802cca164e8b5
Souvik Jana <souvikjana2013@gmail.com>
2021-05-09 20:38:25 +0530

    feat: added controller for user

---
+++ videozen-backend/controllers/user.js
@@ -0,0 +1,63 @@
+const User = require("../models/user");
+
+const { extend } = require("lodash");
+
+exports.getUserById = async (req, res, next, id) => {
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "NOT Found the user" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error occured", errorMessage: error.message });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.json({ savedUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Unable to save user", errorMessage: error.message });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({ users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Unable to get users", errorMessage: error.message });
+  }
+};
+
+exports.getUser = (req, res) => {
+  let { user } = req;
+  user.__v = undefined;
+  res.json({ user });
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    let { user } = req;
+    const userUpdates = req.body;
+    user = extend(user, userUpdates);
+    user = await user.save();
+    res.json({ user });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error occured", errorMessage: error.message });
+  }
+};
---
+++ videozen-backend/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "/.env" });
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const userRoutes = require("./routes/user");
+
+app.use("/v1", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to API of Videozen");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "NOT Found this route on server" });
+});
+
+app.use((req, res, next, error) => {
+  res
+    .status(500)
+    .json({ message: "Error Occured", errorMessage: error.message });
+});
+
+const connectionString = process.env.DB_CONNECTION_STRING;
+
+mongoose
+  .connect(connectionString, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.log("Couldn't connect to DB", error));
+
+const port = process.env.PORT || 8010;
+
+app.listen(port, () =>
+  console.log(`app is running on http://localhost:${port}/`)
+);
