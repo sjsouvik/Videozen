a69832a3ada064cb5ee468889dcd3c8cdb76f74d
Souvik Jana <souvikjana2013@gmail.com>
2021-05-13 15:58:01 +0530

    feat: added routes and controllers for created playlists

--- videozen-backend/models/createdPlaylist.js
+++ videozen-backend/models/createdPlaylist.js
@@ -1,18 +1,22 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
+const playlistSchemaInCreatedPlaylist = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter the name of the playlist"],
+  },
+  videos: [
+    {
+      type: ObjectId,
+      ref: "Video",
+    },
+  ],
+});
+
 const createdPlaylistSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Please enter the name of the playlist"],
-    },
-    videos: [
-      {
-        type: ObjectId,
-        ref: "Video",
-      },
-    ],
+    playlist: [playlistSchemaInCreatedPlaylist],
     user: {
       type: ObjectId,
       ref: "User",
